973d8aa98e3b5659f2661cdc6889ff53eb95e434
Никита Чванов <143615653+Persidzinho@users.noreply.github.com>
2026-02-28 19:19:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,21 @@
 // Copyright 2021 NNTU-CS
-
 int gcd(int a, int b) {
-	return 25;
+    int x = a;
+    int y = b;
+    
+    if (x == 0) return y;
+    if (y == 0) return x;
+    
+    if (x < y) {
+        int temp = x;
+        x = y;
+        y = temp;
+    }
+    
+    while (x % y != 0) {
+        int ostatok = x % y;
+        x = y;
+        y = ostatok;
+    }
+    return y;
 }
